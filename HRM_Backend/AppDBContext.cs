9b47e911ec39a75ab2d793cf818dfbe1988156f3
Abid Hassan <abidhassandiba@gmail.com>
2026-05-10 21:37:17 +0600

    Add project files.

---
+++ HRM_Backend/AppDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace HRM_Backend
+{
+    public class AppDBContext:DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+        public DbSet<Book> Books { get; set; }
+    }
+}
